26745563542b5427b4f63249e7b42205b3bf2cdb
juan <josejuanpatron1630@gmail.com>
2020-02-18 18:49:22 -0600

    Se modifico el modelo del personal

--- models/personal.js
+++ models/personal.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let personalSchema = new Schema({
-  nombre: { type: String, required: [true, 'El nombre es requerido'] },
+  nombre: { type: String, required: [true, 'El nombre es requerido'] }, // nombre completo
   password: { type: String, required:[true, 'La constrasenia es requerida'] },
   RFC:  { type: String, required: [true, 'ElRFC es requerido'] },
-  curp: { type: String, required: [ true, 'El curp es requerido' ] },
+  curp: { type: String, required: [ true, 'El curp es requerido' ] }, // limitado a los 18
   role: { type: String, required: [ true, 'El role es requerido' ]},
   fechaNaciemiento: { type: Date },
-  fechaInicio: { type: Date },
+  fechaInicio: { type: Date }, // esta sera una funcion que se dispare no necesita input
   turno: { type: String, required: [ true, 'El turno es requerid' ] },
   img: { type: String }
 });
